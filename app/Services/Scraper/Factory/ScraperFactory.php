594cd94131b3db384da974d8459178e4153f54d4
Abul Hassan <imabulhasan99@gmail.com>
2025-05-03 20:40:36 +0600

    Enhance ScraperFactory to return AbstractScraper type
    Updated the `create` method in `ScraperFactory` to explicitly declare its return type as `AbstractScraper`. This enforces stricter type safety and improves code clarity and maintainability.

--- app/Services/Scraper/Factory/ScraperFactory.php
+++ app/Services/Scraper/Factory/ScraperFactory.php
@@ -2,13 +2,14 @@
 
 namespace App\Services\Scraper\Factory;
 
+use App\Services\Scraper\AbstractScraper;
 use App\Services\Scraper\Contracts\ScraperInterface;
 use App\Services\Scraper\Parsers\DOMParser;
 use App\Services\Scraper\Strategies\SimplyHired;
 
 class ScraperFactory
 {
-    public function create(string $source)
+    public function create(string $source) : AbstractScraper
     {
         return match ($source) {
             'simply-hired' => new SimplyHired(new DOMParser()),

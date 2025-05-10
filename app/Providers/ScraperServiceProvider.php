835c9739bdb7635396be733d57495c67e3f763d7
Abul Hassan <imabulhasan99@gmail.com>
2025-05-10 15:46:45 +0600

    Refactor ScraperServiceProvider to streamline parser and scraper registrations

--- app/Providers/ScraperServiceProvider.php
+++ app/Providers/ScraperServiceProvider.php
@@ -17,22 +17,18 @@ class ScraperServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        // Register the listing parser
         $this->app->bind('scraper.parser.simplyhired', function ($app) {
             return new DOMParser();
         });
 
-        // Register the job detail parser
         $this->app->bind('scraper.parser.simplyhired.detail', function ($app) {
             return new SimplyHiredJobDetailParser();
         });
 
-        // Register the listing scraper
         $this->app->bind('scraper.simplyhired', function ($app) {
             return new SimplyHired($app->make('scraper.parser.simplyhired'));
         });
 
-        // Register the job detail scraper
         $this->app->bind('scraper.simplyhired.detail', function ($app) {
             return new SimplyHiredJobDetail($app->make('scraper.parser.simplyhired.detail'));
         });

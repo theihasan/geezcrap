a3e85d00c1a78a98d6f27ec07638f15437982e9c
Abul Hassan <imabulhasan99@gmail.com>
2025-05-14 07:22:33 +0600

    feat(config): add job-search.php configuration file and update .env.example
    Add a new configuration file `job-search.php` to define job search URLs for various developer roles and technologies. Also, update `.env.example` to include old database credentials for migration purposes.

---
+++ config/job-search.php
@@ -0,0 +1,229 @@
+<?php
+
+return [
+    'simply-hired' => [
+        'search_urls' => [
+            [
+                'query' => 'php+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'javascript+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'python+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'java+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'full+stack+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'software+engineer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'web+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'backend+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'frontend+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'devops+engineer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'cloud+engineer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'data+engineer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'mobile+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'react+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'angular+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'vue+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'node+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'laravel+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'symfony+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'wordpress+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'drupal+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'magento+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'shopify+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'ruby+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'rails+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'c%23+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => '.net+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'golang+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'rust+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'scala+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'kotlin+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'swift+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'ios+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'android+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'react+native+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'flutter+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'xamarin+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'unity+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'game+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'blockchain+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'solidity+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'smart+contract+developer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'machine+learning+engineer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'ai+engineer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'data+scientist',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'big+data+engineer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'security+engineer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'penetration+tester',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'network+engineer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'system+administrator',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'database+administrator',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'solutions+architect',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'technical+architect',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'qa+engineer',
+                'location' => 'New+York%2C+NY',
+            ],
+            [
+                'query' => 'test+automation+engineer',
+                'location' => 'New+York%2C+NY',
+            ]
+        
+        ]
+    ]
+];

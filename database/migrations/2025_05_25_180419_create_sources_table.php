fc2b2901ed2956b220155582fc69733b5e15a7e5
Abul Hassan <imabulhasan99@gmail.com>
2025-05-26 00:08:45 +0600

    fix:migration error

---
+++ database/migrations/2025_05_25_180419_create_sources_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('sources', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 500);
+            $table->string('url', 255);
+            $table->string('source', 50);
+            $table->timestamp('processed_at')->nullable();
+            $table->unsignedInteger('fail_count')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('sources');
+    }
+};

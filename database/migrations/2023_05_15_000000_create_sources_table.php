d9e1876cf918c5492064844ea62b83491d3dc7f3
Abul Hassan <imabulhasan99@gmail.com>
2025-05-25 23:59:09 +0600

    add sources table

---
+++ database/migrations/2023_05_15_000000_create_sources_table.php
@@ -0,0 +1,26 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    public function up(): void
+    {
+        Schema::create('sources', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 500);
+            $table->string('url', 255);
+            $table->string('source', 50);
+            $table->timestamp('processed_at')->nullable()->after('source');
+            $table->unsignedInteger('fail_count')->default(0)->after('processed_at');
+            $table->timestamps();
+            $table->softDeletes();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('sources');
+    }
+};
